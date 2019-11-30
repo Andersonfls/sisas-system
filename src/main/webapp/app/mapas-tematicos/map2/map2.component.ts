e8c58155565590db173f25e4f8a08c4a1ef33300
Unknown <anderson.frsoares@outlook.com>
2019-11-30 14:54:37 +0000

    mudanca na forma de inicializar o map

--- src/main/webapp/app/mapas-tematicos/map2/map2.component.ts
+++ src/main/webapp/app/mapas-tematicos/map2/map2.component.ts
@@ -7,7 +7,7 @@ import * as CanvasJS from '../../../content/js/canvasjs.min.js';
 import {ActivatedRoute} from '@angular/router';
 import {Municipio, MunicipioService} from '../../entities/municipio/index';
 import {Comuna, ComunaService} from '../../entities/comuna/index';
-import { icon, latLng, Map, marker, point, polyline, tileLayer } from 'leaflet';
+import {icon, latLng, LatLngBounds, Map, marker, point, polyline, tileLayer} from 'leaflet';
 import * as L from 'leaflet';
 
 @Component({
@@ -35,35 +35,14 @@ export class Map2Component implements OnInit {
     predicate: any;
 
     private map;
-    mostrar: boolean;
 
-    streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        detectRetina: true,
-        attribution: '&amp;copy; &lt;a href="https://www.openstreetmap.org/copyright"&gt;OpenStreetMap&lt;/a&gt; contributors'
-    });
-    wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png', {
-        detectRetina: true,
-        attribution: '&amp;copy; &lt;a href="https://www.openstreetmap.org/copyright"&gt;OpenStreetMap&lt;/a&gt; contributors'
-    });
-
-    summit = marker([ 46.8523, -121.7603 ], {
-        icon: icon({
-            iconSize: [ 25, 41 ],
-            iconAnchor: [ 13, 41 ],
-            iconUrl: 'leaflet/marker-icon.png',
-            shadowUrl: 'leaflet/marker-shadow.png'
-        })
-    });
-    paradise = marker([ 46.78465227596462, -121.74141269177198 ], {
-        icon: icon({
-            iconSize: [ 25, 41 ],
-            iconAnchor: [ 13, 41 ],
-            iconUrl: 'leaflet/marker-icon.png',
-            shadowUrl: 'leaflet/marker-shadow.png'
-        })
-    });
-
-    popup: any;
+    options = {
+        layers: [
+            tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+        ],
+        zoom: 4,
+        center: latLng(-12.382928338487396, 42.71484375000001)
+    };
 
     constructor(
         private municipioService: MunicipioService,
@@ -86,7 +65,6 @@ export class Map2Component implements OnInit {
         this.principal.identity().then((account) => {
             this.currentAccount = account;
         });
-        this.initMap();
     }
 
     private initMap(): void {
@@ -102,11 +80,10 @@ export class Map2Component implements OnInit {
 
         tiles.addTo(this.map);
 
-        this.popup = L.popup();
     }
 
     onMapClick(map: Map) {
-       console.log(map.getBounds());
+       console.log(map);
     }
 
     sort() {
