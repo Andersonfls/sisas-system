ae19833bac0592fb4cf157cb1f778e9dc4804ac3
Unknown <anderson.frsoares@outlook.com>
2020-02-10 02:36:32 +0000

    adicao de campos para paginas de contacto e sobre

--- src/main/java/com/minea/sisas/domain/SobreDna.java
+++ src/main/java/com/minea/sisas/domain/SobreDna.java
@@ -39,6 +39,30 @@ public class SobreDna implements Serializable {
     @Column(name = "id_tipo_pagina")
     private Long tipoPagina;
 
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "telemovel", length = 100)
+    private String telemovel;
+
+    @Column(name = "endereco", length = 100)
+    private String endereco;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @Column(name = "primeiro_paragrafo")
+    private String primeiro_paragrafo;
+
+    @Column(name = "segundo_paragrafo")
+    private String segundo_paragrafo;
+
+    @Column(name = "terceiro_paragrafo")
+    private String terceiro_paragrafo;
+
+    @Column(name = "quarto_paragrafo")
+    private String quarto_paragrafo;
+
     // jhipster-needle-entity-add-field - JHipster will add fields here, do not remove
     public Long getId() {
         return id;
@@ -95,6 +119,69 @@ public class SobreDna implements Serializable {
         this.tipoPagina = tipoPagina;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelemovel() {
+        return telemovel;
+    }
+
+    public void setTelemovel(String telemovel) {
+        this.telemovel = telemovel;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getPrimeiro_paragrafo() {
+        return primeiro_paragrafo;
+    }
+
+    public void setPrimeiro_paragrafo(String primeiro_paragrafo) {
+        this.primeiro_paragrafo = primeiro_paragrafo;
+    }
+
+    public String getSegundo_paragrafo() {
+        return segundo_paragrafo;
+    }
+
+    public void setSegundo_paragrafo(String segundo_paragrafo) {
+        this.segundo_paragrafo = segundo_paragrafo;
+    }
+
+    public String getTerceiro_paragrafo() {
+        return terceiro_paragrafo;
+    }
+
+    public void setTerceiro_paragrafo(String terceiro_paragrafo) {
+        this.terceiro_paragrafo = terceiro_paragrafo;
+    }
+
+    public String getQuarto_paragrafo() {
+        return quarto_paragrafo;
+    }
+
+    public void setQuarto_paragrafo(String quarto_paragrafo) {
+        this.quarto_paragrafo = quarto_paragrafo;
+    }
 // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here, do not remove
 
     @Override
